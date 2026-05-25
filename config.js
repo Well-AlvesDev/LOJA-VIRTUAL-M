1d536f46299171a9cac840c6aecfa368acecee6c
Well-alvesDev <weslleykholson@gmail.com>
2026-05-25 20:43:50 -0300

    feat: update payment button styles and remove Mercado Pago checkout script

--- config.js
+++ config.js
@@ -4,3 +4,6 @@ const SUPABASE_CONFIG = {
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhvdmZjbnR6dGhhaHdzempheHN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMDgxNDUsImV4cCI6MjA5Mzc4NDE0NX0.Pss5O_ykTybPUsuZCCln72Pq5dkTGMQ1G1kXR4HOVyw',
     table: 'j-box'
 };
+
+window.MP_PUBLIC_KEY = "APP_USR-19e5b389-ec4e-4236-bf87-88480b38e789";
+window.SUPABASE_EDGE_URL = "https://yoejlumglxbzxtzknsuy.supabase.co/functions/v1/";
